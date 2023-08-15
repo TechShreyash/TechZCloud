41cf9d3cbac7a8309143cef9a1d5f2c2c8cf2d52
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-08-15 19:45:59 +0530

    Update remote_upload.py

--- utils/remote_upload.py
+++ utils/remote_upload.py
@@ -5,4 +5,4 @@ from utils.upload import upload_file_to_channel
 async def start_remote_upload(session, hash, url):
     ext = await download_file(session, hash, url)
     if ext:
-        await upload_file_to_channel(hash, hash + ext, ext)
+        await upload_file_to_channel(hash, hash + "." + ext, ext)
